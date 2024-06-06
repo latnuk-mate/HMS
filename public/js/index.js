d6e8e2b3dd2dd797eb1875597b4795c2f06f93e2
latnuk <kuntalhome999@gmail.com>
2024-06-07 01:08:00 +0530

    customize a function

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,4 @@
 // client side javascript file..
-
-// $(document).ready(function (){
-  
-//     $('.department--list li').click(function(){
-//         $('.department--list li').removeClass('active');
-//         $(this).addClass('active');
-
-//         var filterValue = $(this).attr('data-filter');
-//         $grid.isotope({ filter: filterValue });
-//     });
-
-    
-
-//    var $grid =  $('.grid').isotope({
-//         itemSelector: '.all',
-//         percentPosition: false,
-//         masonry: {
-//           columnWidth: '.all'
-//         }
-//       })
-
-
-// });
-
 $(window).on('load' , function(){
       
     $('.department--list li').click(function(){
